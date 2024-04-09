610ad8c1dc1c4d47affc06b7e0a55027a26462d6
FERNANDO GÓMEZ-LIMÓN RODERO <Fernando.Gomez12@alu.uclm.es>
2024-04-09 18:11:20 +0200

    cambios pilotodao

--- Persistencia/PilotoDAO.vb
+++ Persistencia/PilotoDAO.vb
@@ -8,7 +8,7 @@
     Public Sub LeerTodas()
         Dim p As Piloto
         Dim col, aux As Collection
-        col = AgenteBD.ObtenerAgente().Leer("SELECT * FROM PAISES ORDER BY IdPais") 'llamamos a obtenerAgente que como es shared no tenemos que crear el objeto agente'
+        col = AgenteBD.ObtenerAgente().Leer("SELECT * FROM pilotos ORDER BY IdPiloto") 'llamamos a obtenerAgente que como es shared no tenemos que crear el objeto agente'
         For Each aux In col 'aux es una coleccion que representa a cada elemento de col en el bucle'
             p = New Piloto(aux(1).ToString) 'el primer elemento( axu(1) ) es el ID'
             p.Nombre = aux(2).ToString 'el segundo elemento( aux(2) ) es el nombre'
@@ -20,7 +20,7 @@
 
     Public Sub Leer(ByRef p As Piloto) 'lo mismo que leer todas pero solo una persona la diferencia esta que como parametros pasamos una persona'
         Dim col As Collection : Dim aux As Collection
-        col = AgenteBD.ObtenerAgente.Leer("SELECT * FROM PAISES WHERE IdPais='" & p.IDPiloto & "';") 'el select es diferente al de leer todas le pasamos la id de persona'
+        col = AgenteBD.ObtenerAgente.Leer("SELECT * FROM pilotos WHERE IdPiloto='" & p.IDPiloto & "';") 'el select es diferente al de leer todas le pasamos la id de persona'
         For Each aux In col
             p.Nombre = aux(2).ToString
             p.Apellido = aux(3)
@@ -28,15 +28,15 @@
         Next
     End Sub
     Public Function Insertar(ByVal p As Piloto) As Integer
-        Return AgenteBD.ObtenerAgente.Modificar("INSERT INTO PAISES VALUES ('" & p.IDPiloto & "', '" & p.Nombre & "', '" & p.Apellido & "');")
+        Return AgenteBD.ObtenerAgente.Modificar("INSERT INTO pilotos VALUES ('" & p.IDPiloto & "', '" & p.Nombre & "', '" & p.Apellido & "');")
     End Function
 
     Public Function Actualizar(ByVal p As Piloto) As Integer
-        Return AgenteBD.ObtenerAgente.Modificar("UPDATE PAISES SET NombrePais='" & p.Nombre & "' WHERE IdPais='" & p.IDPiloto & "';")
+        Return AgenteBD.ObtenerAgente.Modificar("UPDATE pilotos SET NombrePiloto='" & p.Nombre & "' WHERE IdPiloto='" & p.IDPiloto & "';")
     End Function
 
     Public Function Borrar(ByVal p As Piloto) As Integer
-        Return AgenteBD.ObtenerAgente.Modificar("DELETE FROM PAISES WHERE IdPais='" & p.IDPiloto & "';")
+        Return AgenteBD.ObtenerAgente.Modificar("DELETE FROM pilotos WHERE IdPiloto='" & p.IDPiloto & "';")
     End Function
 
 End Class
